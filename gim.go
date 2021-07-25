1144c191283682d45457f5413f88a1ddd35ffad3
Dozinj <2746764072@qq.com>
2021-07-25 10:23:10 +0800

    updating

--- gim.go
+++ gim.go
@@ -1,26 +1,24 @@
-package gim
+package Gim
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter,*http.Request)
+type HandlerFunc func(ctx *Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 
 //创建Engine实列
 func New()*Engine{
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 //添加静态路由
 func (engine *Engine)addRouter(method,patten string,handler HandlerFunc){
-	key:=method+"-"+patten
-	engine.router[key]=handler
+	engine.router.addRouter(method,patten,handler)
 }
 
 //添加GET路由
@@ -34,13 +32,10 @@ func (engine *Engine)POST(patten string,handler HandlerFunc){
 }
 
 func (engine *Engine)ServeHTTP(w http.ResponseWriter,r *http.Request){
-	key:=r.Method+"-"+r.URL.Path
-	if hanlder,ok:= engine.router[key];ok{
-		hanlder(w,r)
-	}else{
-		fmt.Fprintf(w,"404 NOT FOUND %s\n",r.URL)
-	}
+	c:=newContext(w,r)
+	engine.router.handler(c)
 }
+
 
 func (engine *Engine)Run(addr string)error{
 	return http.ListenAndServe(addr,engine) //只要传入任何实现了 ServerHTTP 接口的实例，所有的HTTP请求，就都交给了该实例处理了
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module "gim"
+module "Gim"
+
+
